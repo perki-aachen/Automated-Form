ad75a0c37fcbf462681cfa22781b5535cc1fb713
felixarjuna <felixarjuna.de@yahoo.com>
2023-01-14 00:26:13 +0100

    🎉 Simplify code

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,24 @@
+import datetime
 from datetime import datetime as dt
-import json
-from automated_form import duplicate_form, get_next_saturday, update_form, send_message_to_line
+
+from automated_form import (
+    get_next_saturday,
+    create_form,
+    send_message_to_line,
+)
 
 
 def lambda_handler():
-    today = dt.today()
-    week = today.strftime("%U")
+    today: datetime.date = dt.today()
+    week: str = today.strftime("%U")
+    satur_date: datetime.date = get_next_saturday(today, 5)
+    str_date: str = satur_date.strftime("%d %B %Y")
 
-    # Duplicate Form
-    result = duplicate_form(week=week)
-    print(json.dumps(result, indent=2))
-
-    # Update Form
-    satur_date = get_next_saturday(today, 5)
-    str_date = satur_date.strftime("%d %B %Y")
-    res = update_form(date=str_date, result=result)
-    print(json.dumps(res, indent=2))
-    res_uri = res["responderUri"]
-    print(res_uri)
+    # Create a new form
+    URI = create_form(week, str_date)
 
     # Send Message to Line
-    send_message_to_line(res_uri)
+    send_message_to_line(URI)
 
 
 lambda_handler()
